fa6e20ddf8ba068e368a0f3fd031cd48e845a6e4
Ethan-Zheng <guantianzheng136@gmail.com>
2025-09-19 20:01:54 +0800

    9.19

---
+++ assets/gif.py
@@ -0,0 +1,4 @@
+from moviepy import VideoFileClip
+
+clip = VideoFileClip("/Users/ethan-zheng/Desktop/UMPE/assets/bed67ef03c4a4066a74f6c0117d512ee_video.mp4")
+clip.write_gif("/Users/ethan-zheng/Desktop/UMPE/assets/bed67ef03c4a4066a74f6c0117d512ee_video.gif")
