45fcb1d8ade9503713c152bbfe3ee87fd1daf547
Ethan-Zheng <guantianzheng136@gmail.com>
2025-09-19 20:18:58 +0800

    9.19

--- README.md
+++ README.md
@@ -83,7 +83,7 @@ second.”
 ## Our results
 **MAPPING RESULTS ON NUSCENES VALIDATION DATASET**
 ![mainable](assets/maintable.jpg)
-![vis](assets/all_final_video.gif)
+![vis](assets/all_final_video.mp4)
 ![vis](assets/bed67ef03c4a4066a74f6c0117d512ee_video.mp4)
 
 **MAPPING RESULTS ON ARGOVERSE 2 VALIDATION DATASET**  
--- assets/gif.py
+++ assets/gif.py
@@ -1,4 +1,8 @@
 from moviepy import VideoFileClip
 
-clip = VideoFileClip("/Users/ethan-zheng/Desktop/UMPE/assets/bed67ef03c4a4066a74f6c0117d512ee_video.mp4")
-clip.write_gif("/Users/ethan-zheng/Desktop/UMPE/assets/bed67ef03c4a4066a74f6c0117d512ee_video.gif")
+clip = VideoFileClip("/Users/ethan-zheng/Desktop/UMPE/assets/all_final_video.mp4")
+clip.write_gif(
+    "/Users/ethan-zheng/Desktop/UMPE/assets/all_final_video.gif",
+    fps=5
+)
+
